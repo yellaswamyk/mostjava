fee2004b4774edd0c2a6d07b8bd397b5182be8e6
yellaswamy <yellaswamymtech@gmail.com>
2023-04-16 16:37:10 +0530

    Initial commit welcome Java.

---
+++ CoreJava/01-WelcomeJava/src/com/most/corejava/Welcome.java
@@ -0,0 +1,10 @@
+package com.most.corejava;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welecome I & Eye Students for Summer Java Developer Course");
+
+	}
+
+}
