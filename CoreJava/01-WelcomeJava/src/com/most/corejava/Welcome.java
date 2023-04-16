76fb6a936b899b0824f692b179cc456b07a9f8d3
yellaswamyk <yellaswamymtech@gmail.com>
2023-04-17 00:53:19 +0530

    Testing......yellaswamy

--- CoreJava/01-WelcomeJava/src/com/most/corejava/Welcome.java
+++ CoreJava/01-WelcomeJava/src/com/most/corejava/Welcome.java
@@ -6,7 +6,7 @@ public class Welcome {
 		System.out.println("Welecome I & Eye Students for Summer Java Developer Course");
 		System.out.println("Welcome Vishnu Git Test");
 		System.out.println("Welcome Vishnu Git Test for new branch created most/yellaswamy");
-
+		System.out.println("Teting.......");
 	}
 
 }
