e64f8c9559beb534fd84ca4c1cb92f123eaab396
yellaswamyk <yellaswamymtech@gmail.com>
2023-04-16 22:11:02 +0530

    Added new print statement for Testing git.

--- CoreJava/01-WelcomeJava/src/com/most/corejava/Welcome.java
+++ CoreJava/01-WelcomeJava/src/com/most/corejava/Welcome.java
@@ -4,6 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Welecome I & Eye Students for Summer Java Developer Course");
+		System.out.println("Welcome Vishnu Git Test");
 
 	}
 
