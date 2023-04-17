8371f4540d058abe7faebae9af3a2a3336d76c8c
rvishnu2 <vishnu.rayarakula@gmail.com>
2023-04-16 23:18:07 -0700

    Changed error log

--- CoreJava/02-vishnuangular/ng15hospital/src/app/appmodule.ts
+++ CoreJava/02-vishnuangular/ng15hospital/src/app/appmodule.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AdminModule } from "src/admin/adminmodule";
-import { AuthModule } from "src/auth/authModule";
+import { AuthModule } from "src/auth/authmodule";
 import { DoctorModule } from "src/doctor/doctormodule";
 import { AppComponent } from "./appcomponent";
 @NgModule({
