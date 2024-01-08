9423437d48208a35dd4e4101b9b5e973309c3302
Admin <Admin@DESKTOP-FUE2678>
2024-01-08 07:32:33 +0530

    inheritencedemo2 project files added

--- most202312j01-kavya/InheritenceDemo2/src/com/most/Child.java
+++ most202312j01-kavya/InheritenceDemo2/src/com/most/Child.java
@@ -1,5 +1,9 @@
 package com.most;
 
-public class Child {
-
+public class Child extends Parent {
+public Child(int x, int y) {
+	super(x,y)
+	System.out.println("inside child class constructor");
+	
+}
 }
